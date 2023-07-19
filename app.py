31c0adde7562ecddb1f4b036684f64d5e173f12b
Christopher Dykes <chris.t.c.dykes@gmail.com>
2023-07-19 17:33:32 -0400

    Add language query

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import psycopg2
 
 # instance of flask
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return 'Hi mom'
 
-
 @app.route('/language')
 def get_languages():
     conn = psycopg2.connect(
-        database="multilingual",
+        database='multilingual',
         user='guest',
         password='coolpasswordbro',
         host='192.46.222.118',
@@ -24,3 +22,21 @@ def get_languages():
     result = cursor.fetchall()
     return jsonify(result)
 
+@app.route('/item')
+def get_items():
+    conn = psycopg2.connect(
+        database='multilingual',
+        user='guest',
+        password='coolpasswordbro',
+        host='192.46.222.118',
+        port='5432'
+    )
+    cursor = conn.cursor()
+    language = request.args.get('language')
+    if language is None:
+        language = 'en'
+    cursor.execute(f'''select * from items
+        join description_translations dt on items.id = dt.item_id
+        where dt.language_code = \'{language}\'''')
+    result = cursor.fetchall()
+    return jsonify(result)
--- sql/init.sql
+++ sql/init.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS languages, items, item_translations;
+DROP TABLE IF EXISTS languages, items, item_translations, name_transaltions, description_translations;
 
 CREATE TABLE languages (
   language_code CHAR(2) PRIMARY KEY, 
@@ -8,13 +8,17 @@ CREATE TABLE languages (
 INSERT INTO languages (language_code, language_name) VALUES
   ('en', 'English'),
   ('fr', 'French'),
-  ('tr', 'Turkish'):
+  ('tr', 'Turkish');
+
+GRANT SELECT ON languages TO guest;
 
 CREATE TABLE items (
   id SERIAL PRIMARY KEY,
   cost DECIMAL(12,2) NOT NULL
 );
-  
+
+GRANT SELECT ON items TO guest;
+
 CREATE TABLE item_translations (
   id SERIAL PRIMARY KEY,
   item_id INT NOT NULL,
@@ -36,4 +40,34 @@ INSERT INTO item_translations (item_id, language_code, field, translation) VALUE
   (1, 'tr', 'name', 'Logitech M510 Kablosuz Bilgisayar Faresi'),
   (1, 'tr', 'description', 'Bu ergonomik tasarımlı fare ile eliniz saatler sonra bile rahat kalır. Yumuşak kauçuk kulplu konturlu şekli, hafif kıvrımlı kenarları ve geniş avuç içi alanı, gün boyu zahmetsiz kontrol için ihtiyacınız olan desteği sağlar.');
 
-GRANT SELECT ON languages, items, item_translations TO guest;
+GRANT SELECT ON item_translations TO guest;
+
+CREATE TABLE name_translations (
+    id SERIAL PRIMARY KEY,
+    item_id INT NOT NULL,
+    language_code CHAR(2) NOT NULL,
+    translation TEXT NOT NULL,
+    FOREIGN KEY (item_id) REFERENCES items(id),
+    FOREIGN KEY (language_code) REFERENCES languages(language_code)
+);
+
+INSERT INTO name_translations (item_id, language_code, translation) VALUES
+    (1, 'en', 'Logitech M510 Wireless Computer Mouse'),
+    (1, 'fr', 'Logitech M510 Souris d''ordinateur'),
+    (1, 'tr', 'Logitech M510 Kablosuz Bilgisayar Faresi');
+
+CREATE TABLE description_translations (
+    id SERIAL PRIMARY KEY,
+    item_id INT NOT NULL,
+    language_code CHAR(2) NOT NULL,
+    translation TEXT NOT NULL,
+    FOREIGN KEY (item_id) REFERENCES items(id),
+    FOREIGN KEY (language_code) REFERENCES languages(language_code)
+);
+
+INSERT INTO description_translations (item_id, language_code, translation) VALUES
+    (1, 'en', 'Your hand can relax in comfort hour after hour with this ergonomically designed mouse. Its contoured shape with soft rubber grips, gently curved sides and broad palm area give you the support you need for effortless control all day long.'),
+    (1, 'fr', 'Votre main peut se détendre confortablement heure après heure avec cette souris ergonomique. Sa forme profilée avec des poignées en caoutchouc souple, des côtés légèrement incurvés et une large zone de paume vous offrent le soutien dont vous avez besoin pour un contrôle sans effort tout au long de la journée.'),
+    (1, 'tr', 'Bu ergonomik tasarımlı fare ile eliniz saatler sonra bile rahat kalır. Yumuşak kauçuk kulplu konturlu şekli, hafif kıvrımlı kenarları ve geniş avuç içi alanı, gün boyu zahmetsiz kontrol için ihtiyacınız olan desteği sağlar.');
+
+GRANT SELECT ON name_translations, description_translations TO guest;
